b38d8b6146c12f15c8df4ed3aa980364bef28933
MR-GOTHAM <146982036+MR-GOTHAM@users.noreply.github.com>
2024-10-18 15:45:06 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('alertBtn')
+
+btn.addEventListener('click',() => {
+    alert('You are about to make a purchase of 1FORYOU Voucher, You will be taken to a confirmation page')
+})
